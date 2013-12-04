c41a1c1e41344aa9e1d32200f8a5741a44456e74
Lucas Brendel <lucasbrendel@live.com>
2013-12-03 21:43:18 -0600

    Added new interface files and a Settings View

---
+++ Badger/ViewModels/BadgerSettingsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Badger.ViewModels
+{
+    public class BadgerSettingsViewModel : PropertyChangedBase, IBadgerSettingsViewModel
+    {
+        private string _displayName = "Badger Settings";
+
+        #region IHaveDisplayName Members
+
+        public string DisplayName
+        {
+            get
+            {
+                return _displayName;
+            }
+            set
+            {
+                if (value != _displayName)
+                {
+                    _displayName = value;
+
+                    NotifyOfPropertyChange(() => DisplayName);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Badger/Views/BadgerSettingsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Badger.Views
+{
+    /// <summary>
+    /// Interaction logic for BadgerSettingsView.xaml
+    /// </summary>
+    public partial class BadgerSettingsView : Window
+    {
+        public BadgerSettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
