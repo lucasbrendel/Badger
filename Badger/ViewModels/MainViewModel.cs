416014ee0d13ce43428effde4c338974a9369d2d
Lucas Brendel <lucasbrendel@live.com>
2013-12-03 19:11:55 -0600

    Forgot to add the View Model

---
+++ Badger/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using System.ComponentModel.Composition;
+
+namespace Badger.ViewModels
+{
+    
+    public class MainViewModel : PropertyChangedBase, IHaveDisplayName
+    {
+        private string _displayName = "Badger";
+
+        #region IHaveDisplayName Members
+
+        /// <summary>
+        /// The Title of the View
+        /// </summary>
+        public string DisplayName
+        {
+            get
+            {
+                return _displayName;
+            }
+            set
+            {
+                if (value != _displayName)
+                {
+                    _displayName = value;
+
+                    NotifyOfPropertyChange(() => DisplayName);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
