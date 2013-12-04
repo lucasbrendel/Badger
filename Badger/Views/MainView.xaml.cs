98e00c4cc13cacb1aa0021aa400e6aa50a2a6ab9
Lucas Brendel <lucasbrendel@live.com>
2013-12-03 21:17:43 -0600

    Removed the the Extended View and changed the namespace on Main View

--- Badger/Views/MainView.xaml.cs
+++ Badger/Views/MainView.xaml.cs
@@ -10,14 +10,13 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 
-namespace Badger
+namespace Badger.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainView.xaml
     /// </summary>
     public partial class MainView : MetroWindow
     {
