f6800fe7052032c94f438dbc98c32ee1733aa898
Lucas Brendel <lucasbrendel@live.com>
2013-12-03 21:01:13 -0600

    Added Extended View Model

---
+++ Badger/ViewModels/BadgerExtendedViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Caliburn.Micro;
+
+namespace Badger.ViewModels
+{
+    [Export(typeof(BadgerExtendedViewModel))]
+    public class BadgerExtendedViewModel : PropertyChangedBase, IHaveDisplayName
+    {
+        private string _displayName = "Extended Badger";
+
+        #region IHaveDisplayName Members
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string DisplayName
+        {
+            get
+            {
+                return _displayName;
+            }
+            set
+            {
+                if (value != _displayName)
+                {
+                    _displayName = value;
+
+                    NotifyOfPropertyChange(() => DisplayName);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
