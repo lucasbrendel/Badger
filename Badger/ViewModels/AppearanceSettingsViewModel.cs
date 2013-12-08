83aa6f809db589441e59c44de2f7e0b1a0bc0a73
Lucas Brendel <lucasbrendel@live.com>
2013-12-08 10:28:56 -0600

    New Settings class for changing the appearance

---
+++ Badger/ViewModels/AppearanceSettingsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using System.ComponentModel.Composition;
+
+namespace Badger.ViewModels
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Export(typeof(IBadgerSettingsItemViewModel))]
+    public class AppearanceSettingsViewModel : IBadgerSettingsItemViewModel, IHaveDisplayName
+    {
+        private string _displayName;
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        [ImportingConstructor]        
+        public AppearanceSettingsViewModel()
+        {
+            this.DisplayName = "Appearance";
+        }
+
+
+
+        #region IHaveDisplayName Members
+
+        public string DisplayName
+        {
+            get
+            {
+                return _displayName;
+            }
+            set
+            {
+                _displayName = value;
+            }
+        }
+
+        #endregion
+    }
+}
