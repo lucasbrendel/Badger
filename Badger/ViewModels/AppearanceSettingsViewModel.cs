789a1b66080eb4d6dd5d6b3dd1a07b06b2bdd213
Lucas Brendel <lucasbrendel@live.com>
2013-12-08 23:13:45 -0600

    Appearance settings display all supported accent colors and allow switching

--- Badger/ViewModels/AppearanceSettingsViewModel.cs
+++ Badger/ViewModels/AppearanceSettingsViewModel.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
 using System.ComponentModel.Composition;
+using System.Windows.Input;
+using MahApps.Metro;
+using System.Windows.Media;
+using System.Windows;
+using Badger.Commands;
 
 namespace Badger.ViewModels
 {
@@ -12,9 +17,9 @@ namespace Badger.ViewModels
     /// 
     /// </summary>
     [Export(typeof(IBadgerSettingsItemViewModel))]
-    public class AppearanceSettingsViewModel : IBadgerSettingsItemViewModel, IHaveDisplayName
+    public class AppearanceSettingsViewModel : BadgerSettingsItemViewModel
     {
-        private string _displayName;
+        private List<AccentColorData> _accentColors;
 
         /// <summary>
         /// Default Constructor
@@ -23,24 +28,46 @@ namespace Badger.ViewModels
         public AppearanceSettingsViewModel()
         {
             this.DisplayName = "Appearance";
+
+            this._accentColors = ThemeManager.DefaultAccents
+                                            .Select(a => new AccentColorData() { Name = a.Name, ColorBrush = a.Resources["AccentColorBrush"] as Brush })
+                                            .ToList();
         }
 
-
-
-        #region IHaveDisplayName Members
-
-        public string DisplayName
+        /// <summary>
+        /// List of all accent colors supported by MahApps.Metro
+        /// </summary>
+        public List<AccentColorData> AccentColors
         {
-            get
-            {
-                return _displayName;
-            }
+            get { return _accentColors; }
             set
             {
-                _displayName = value;
+                if (value != _accentColors)
+                {
+                    _accentColors = value;
+                }
             }
         }
+        
+    }
 
-        #endregion
+    public class AccentColorData
+    {
+        public string Name { get; set; }
+        public Brush ColorBrush { get; set; }
+
+        private ICommand changeAccentCommand;
+
+        public ICommand ChangeAccentCommand
+        {
+            get { return this.changeAccentCommand ?? (changeAccentCommand = new SimpleCommand { CanExecuteDelegate = x => true, ExecuteDelegate = x => ChangeAccent(x) }); }
+        }
+
+        private void ChangeAccent(object sender)
+        {
+            var theme = ThemeManager.DetectTheme(Application.Current);
+            var accent = ThemeManager.DefaultAccents.First(x => x.Name == this.Name);
+            ThemeManager.ChangeTheme(Application.Current, accent, theme.Item1);
+        }
     }
 }
