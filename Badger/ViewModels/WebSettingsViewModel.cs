4241a3fdde7e6715f300f47a4bc6d912b7e1048c
Lucas Brendel <lucasbrendel@live.com>
2013-12-08 23:13:11 -0600

    Added second settings set for configuring web properties

---
+++ Badger/ViewModels/WebSettingsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+namespace Badger.ViewModels
+{
+    [Export(typeof(IBadgerSettingsItemViewModel))]
+    public class WebSettingsViewModel : BadgerSettingsItemViewModel
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [ImportingConstructor]
+        public WebSettingsViewModel()
+        {
+            this.DisplayName = "Web";
+        }
+    }
+}
