f81a00219e8b3029e094cb3e648b794dddf9e170
Lucas Brendel <lucasbrendel@live.com>
2013-12-09 21:54:07 -0600

    sytem settings manager and interface for implementers

---
+++ Badger/Managers/ISystemSettingLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badger.Managers
+{
+    public interface ISystemSettingLoader
+    {
+        string Save(string key, object value);
+
+        object Load(string key);
+    }
+}
---
+++ Badger/Managers/SystemSettingsManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akavache;
+
+namespace Badger.Managers
+{
+    public class SystemSettingsManager
+    {
+        public SystemSettingsManager()
+        {
+            BlobCache.ApplicationName = "Badger";
+        }
+
+        public static void Save(string key, object value)
+        {
+            BlobCache.LocalMachine.InsertObject(key, value);
+        }     
+  
+        public static void LoadAsync(string key)
+        {
+            //var val = await BlobCache.LocalMachine.GetObjectAsync<object>(key);
+        }
+    }
+}
